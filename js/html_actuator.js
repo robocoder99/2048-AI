cf9b600f779dd1d6409eea02582e04339b354fd0
robocoder99 <admin@www.altech.tk>
2014-05-03 13:42:11 -0400

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -108,15 +108,7 @@ HTMLActuator.prototype.updateScore = function (score) {
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!"
-
-  // if (ga) ga("send", "event", "game", "end", type, this.score);
-
-  this.messageContainer.classList.add(type);
-  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
-
-  this.clearContainer(this.sharingContainer);
-  this.sharingContainer.appendChild(this.scoreTweetButton());
-  twttr.widgets.load();
+  if(won)this.clearMessage();
 };
 
 HTMLActuator.prototype.clearMessage = function () {
